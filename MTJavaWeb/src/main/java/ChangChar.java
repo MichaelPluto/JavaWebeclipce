c76f6ee3dae62cc759cccebbe1adb18fa5bcb7e9
PC-202211211726\Administrator <michaelpluto2000@outlook.com>
2022-12-04 21:47:22 +0800

    1

---
+++ MTJavaWeb/src/main/java/ChangChar.java
@@ -0,0 +1,15 @@
+public class ChangChar{
+	//编写一个main方法
+	//演示转义字符的作用；实例
+	public static void main(String[] args) {
+		System.out.println("北京\t天津\t上海\t ");
+		System.out.println("jack\nsmith\nmary");
+		System.out.println("C:\\Windows\\System32\\cmd.exe");	
+		System.out.println("C:\\\\Windows\\\\System32\\\\cmd.exe");
+		System.out.println("我说:\"要好好学习java有前途\"");
+		System.out.println("我说:\'为了去日本工作\'");
+		System.out.println("我说:\"要好好学习java有前途\"");
+
+
+	}
+}
